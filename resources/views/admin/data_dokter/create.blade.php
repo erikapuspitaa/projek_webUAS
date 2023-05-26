b94d7c5adf8bc3e228f7e8d9341d3a6304373963
brineu <rantisabrina24@gmail.com>
2023-05-26 15:12:23 +0700

    balik awal

--- resources/views/admin/data_dokter/create.blade.php
+++ resources/views/admin/data_dokter/create.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Data Dokter')
 
 @section('content')
-  <!-- Page Heading COBAKK -->
+  <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800">Data Dokter</h1>
 
   <form action = "/data_pasien" method="POST">
